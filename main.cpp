1fba587b44d0673af678583a1ee377f8d066ce35
johnmendozai5 <jmendoz223@gmail.com>
2026-06-16 19:07:29 -0500

    Step 3-4: add Croatia, Egypt, France lines

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// this code was cooked up by Johnny Mendoza on VS Code
+// the code base it available on my GitHub
 #include <iostream>
 
 int main(){
@@ -14,4 +16,8 @@ int main(){
     // PRINT THE U.S LINE
     std::cout << "U.S.    +1 (" << areaCode << ")" << prefix << "-" << linNumber << std::endl;
     std::cout << "Brazil  +55 (" << areaCode << ")" << (prefix + 100) << "-" << linNumber << std::endl;
+    std::cout << "Croatia +385 (" << areaCode << ")" << prefix << "-" << (linNumber + 50) << std::endl;
+    std::cout << "Egypt   +20 (" << (areaCode + 30) << ")" << prefix << "-" << linNumber << std::endl;
+    std::cout << "France  +33 (" << prefix << ")" << areaCode << "-" << linNumber << std::endl;
+
 }
